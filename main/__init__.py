14e67ee0976f766e273974d50bf4fcd0961c20b9
Pupun2542 <ppupun2542@gmail.com>
2021-12-24 21:59:18 +0700

    made login page inheritable

--- main/__init__.py
+++ main/__init__.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.secret_key = "123123123"
+app.secret_key = "9691e12715cf4959ccb52384028c2b0c"
 
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
@@ -13,3 +13,4 @@ app.config["MYSQL_DB"] = "commuthor"
 db = MySQL(app)
 
 from main import routes
+
