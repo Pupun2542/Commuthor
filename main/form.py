579c2720cd34b6f3ebf30e6d67cd17387d25bd76
Cornelios <50621324+Cornelios@users.noreply.github.com>
2021-12-27 00:08:32 +0700

    update home

--- main/form.py
+++ main/form.py
@@ -1,11 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from main import backend
+from main.model import user
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='username', validators=[Length(min=2, max=32), DataRequired()])
-    email = StringField(label='email', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='password', validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label='confirm password', validators=[EqualTo('password1'), DataRequired()])
+    def validate_username(self, username_to_check):
+        ss = user.query.filter_by(UserName=username_to_check.data).first()
+        if ss:
+            raise ValidationError('Username already exists! Please try a different username')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = user.query.filter_by(Email=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists! Please try a different email address')
+
+    username = StringField(label='Username', validators=[Length(min=2, max=32)])
+    email = StringField(label='Email', validators=[Email()])
+    password1 = PasswordField(label='Password', validators=[Length(min=6)])
+    password2 = PasswordField(label='Confirm Password', validators=[EqualTo('password1')])
     submit = SubmitField(label='Sign up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[Email()])
+    password = PasswordField(label='Password', validators=[Length(min=6)])
+    submit = SubmitField(label='Sign in')
+
+
