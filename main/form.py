ef12115f8c235f9f072c5bbb1d02d115542d6bb9
Pupun2542 <ppupun2542@gmail.com>
2021-12-28 00:53:35 +0700

    add profile route

--- main/form.py
+++ main/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from main import backend
 from main.model import user
@@ -30,3 +30,16 @@ class LoginForm(FlaskForm):
     rememberme = BooleanField(label='Remember Me')
 
 
+class ProfileForm(FlaskForm):
+    username = StringField(label='Username', validators=[Length(min=2, max=32)])
+    email = StringField(label='Email', validators=[Email()])
+    SMLink = StringField(label='Social Media Link', validators=[Length(max=255)])
+    Privacy = StringField(label='Privacy')
+    # Profilepic = FileField(label='profile picture')
+    submit = SubmitField(label='Save')
+
+
+class ChangePasswordForm(FlaskForm):
+    password1 = PasswordField(label='Password', validators=[Length(min=6)])
+    password2 = PasswordField(label='Confirm Password', validators=[EqualTo('password1')])
+    submit = SubmitField(label='Save')
